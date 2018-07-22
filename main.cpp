ea6ce78270a6a3d1f5980c4db8723e5e2e3453b1
linqiaozhi <george.linderman@gmail.com>
2018-07-22 17:52:00 -0400

    added 1d

--- main.cpp
+++ main.cpp
@@ -14,6 +14,84 @@ double squared_cauchy(double x, double y) {
 double squared_cauchy_2d(double x1, double x2, double y1, double y2) {
 	return pow(1.0 + pow(x1 - y1, 2) + pow(x2 - y2, 2), -2);
 }
+int test1d(int ndim) {
+
+	//Initialize some charges and locations
+	int n = 1E4;
+	int n_interpolation_points = 5;
+	auto n_boxes_per_dim = 600;
+	double xmin = -50;
+	double xmax = 50;
+
+	double * xs =(double*) malloc(n* sizeof(double));
+	double * charges =(double*) malloc(ndim*n* sizeof(double));
+
+	srand(0);
+	for (int i=0; i< n; i++){
+		xs[i] = (rand() /(double) RAND_MAX - 0.5)*(xmax - xmin);
+		for (int idim = 0; idim<ndim; idim++){
+			charges[i*ndim+idim] = rand() /(double) RAND_MAX;
+		}
+
+	}
+
+	int n_boxes = n_boxes_per_dim * n_boxes_per_dim;
+
+	auto *box_lower_bounds = new double[ n_boxes];
+	auto *box_upper_bounds = new double[ n_boxes];
+	auto *y_tilde_spacings = new double[n_interpolation_points];
+	int n_interpolation_points_1d = n_interpolation_points *n_boxes;
+	auto *x_tilde = new double[n_interpolation_points_1d]();
+	auto *fft_kernel_tilde = new complex<double>[2 * n_interpolation_points_1d ];
+
+	auto *pot = new double[n * ndim]();
+	clock_t begin = clock();
+	precompute(xmin, xmax, n_boxes_per_dim, n_interpolation_points,
+			&squared_cauchy,
+			box_lower_bounds, box_upper_bounds, y_tilde_spacings, x_tilde,  fft_kernel_tilde);
+	clock_t end1 = clock();
+	nbodyfft(n, ndim, xs,  charges, n_boxes_per_dim, n_interpolation_points,  box_lower_bounds,
+			box_upper_bounds, y_tilde_spacings,x_tilde, fft_kernel_tilde, pot);
+	clock_t end2 = clock();
+	double time_spent1 = (double)(end1 - begin) / CLOCKS_PER_SEC;
+	double time_spent2 = (double)(end2 - end1) / CLOCKS_PER_SEC;
+	printf("Precompute: %2e\nFast: %.2e seconds, %.2e per second\n", time_spent1, time_spent2, n/time_spent2);
+
+	kernel_type kernel = &squared_cauchy;
+
+	//Brute force compute for at most 100 points
+	int ntest = fmin(100,n);
+	double * truepot =  (double*) calloc(ntest*ndim,sizeof(double));
+	for (int idim=0; idim<ndim;idim++){
+		for (int i = 0; i< ntest; i++){
+			for (int j = 0; j< n; j++){
+				truepot[i*ndim+idim] += charges[j*ndim+idim]*kernel( xs[i],  xs[j]);
+			}
+		}
+	}
+	double diffnorm = 0;
+	double norm = 0;
+	for (int i=0; i<ntest;i++){
+		for (int j=0; j<ndim;j++){
+			norm += fabs(truepot[i*ndim+j]);
+			diffnorm += fabs(truepot[i*ndim+j]- pot[i*ndim+j]);
+			if (i<10) {
+		//	printf("%d,%d:pot  %f, true pot %f \n", i,j, pot[i*ndim+j], truepot[i*ndim+j]);
+			}
+		}
+	}
+	norm /= (ntest*ndim);
+	diffnorm /=(ntest*ndim);
+
+	printf("%.2e L1 norm of difference \n", diffnorm);
+	printf("%.2e L1 norm of difference divided by L1 norm of the true matrix\n", diffnorm/norm);
+
+
+
+
+	return 0;
+
+}
 int test2d(int ndim) {
 
 	//Initialize some charges and locations
@@ -99,9 +177,16 @@ int test2d(int ndim) {
 }
 
 int main() {
-	printf("Computing one potential:\n");
+
+
+	printf("Computing one potential in 1D:\n");
+	test1d(1);
+	printf("Computing five potentials in 1D:\n");
+	test1d(5);
+
+	printf("Computing one potential in 2D:\n");
 	test2d(1);
-	printf("Computing five potentials:\n");
+	printf("Computing five potentials in 2D:\n");
 	test2d(5);
 
 	return 1;
